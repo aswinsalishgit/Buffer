566e17adf084c41a4c07ac153bbd985fd6e3c34f
aswinsalishgit <aswin25cs@cce.edu.in>
2026-05-24 12:53:08 +0530

    Implement core-splashscreen API to fix white flash on startup; set dark background and vector icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.core:core-splashscreen:1.0.1")
     
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
